ecb09f377d0062aa45088fb0d39633f666ff4f79
jdluis <jdluisdev@gmail.com>
2024-07-08 14:05:44 +0200

    structure of chat, add and members

--- app/(event)/add-item.tsx
+++ app/(event)/add-item.tsx
@@ -1,14 +1,33 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { SafeAreaView, ScrollView, Text } from 'react-native'
+import React, { useState } from 'react'
+import FormTextField from '@/components/FormTextField'
 
-const Add = () => {
+const AddItem = () => {
+  const [form, setForm] = useState({
+    name: "",
+    creator: "",
+    category: [],
+    tags: [],
+    event: "",
+    quantity: 0
+  })
+
   return (
-    <View>
-      <Text>Add Item</Text>
-    </View>
+    <SafeAreaView className='bg-primary h-full'>
+      <ScrollView className='px-4 my-6'>
+        <Text className='text-2xl text-white'>Bring Item</Text>
+
+        <FormTextField
+          type='text'
+          title={'Item Name'}
+          value={form.name}
+          placeholder='Give your event a catch title...'
+          handleChangeText={(e: string) => setForm({ ...form, name: e })}
+        />
+
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
-export default Add
-
-const styles = StyleSheet.create({})
+export default AddItem
--- app/(event)/members.tsx
+++ app/(event)/members.tsx
@@ -1,10 +1,34 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import MapView, { Marker } from 'react-native-maps'
 
 const Members = () => {
+  const [eventData, seteventData] = useState()
   return (
     <View>
       <Text>Members: list, add, invited etc..</Text>
+      <View>
+
+        <MapView
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          className='w-full h-full'
+        >
+          <Marker
+            key={"01"}
+            coordinate={{
+              latitude: 37.78825,
+              longitude: -122.4324,
+            }}
+            title={"Titulo del tesing mapa"}
+            description={"Testeando mapa, descripcion del mapa"}
+          />
+        </MapView>
+      </View>
     </View>
   )
 }
